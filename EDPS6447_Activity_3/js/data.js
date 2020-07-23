272dfc4dda8b00b850abb4b57acd321c38a991f2
reecebr <59673420+reecebr@users.noreply.github.com>
2020-07-23 13:19:19 -0600

    Add files via upload

---
+++ EDPS6447_Activity_3/js/data.js
@@ -0,0 +1,31 @@
+var array_questions = [
+	{
+		statement: "What camera control is similiar to the iris?",
+		topic: "Photography",
+		sub_topic: "controls",
+		answer_key: "option3",
+		hint: "https://www.quora.com/How-is-cameras-aperture-similar-to-the-Iris-in-a-human-eye",
+        label: ["ISO","Shutter Speed","Aperture"],
+        video: {display: true, src: "https://www.youtube.com/embed/Gh1vUu8qcZw"},
+	},
+	{
+		statement: "What does ISO in photography stand for?",
+		topic: "Photograpy",
+		sub_topic: "controls",
+		answer_key: "option2",
+		hint: "https://wistia.com/learn/production/what-is-iso",
+        label: ["In Search Of","International Organization for Standardization","Independent System Operator"],
+        video: {display: false, src: ""},
+	},
+    {
+		statement: "What is the best shutter speed to shoot a sports game in? ",
+		topic: "Photograpy",
+		sub_topic: "controls",
+		answer_key: "option1",
+		hint: "https://photography.tutsplus.com/tutorials/8-tips-for-taking-sports-photos-like-a-pro--photo-296#:~:text=Professional%20sports%20photographers%20use%20a,film%20speed)%20of%20your%20camera.",
+        label: ["1/1000","1/500","1/100"],
+        video: {display: true, src: "https://www.youtube.com/embed/iMVHloCjot4"},
+	},
+];
+
+var array_answers = [];
---
+++ EDPS6447_Activity_3/js/listener.js
@@ -0,0 +1,19 @@
+btn_start.addEventListener("click", function () {
+	routeView("quiz");
+});
+btn_submit.addEventListener("click", function () {
+	getAnswer();
+});
+btn_previous.addEventListener("click", function () {
+	decrementQuestion();
+});
+btn_hint.addEventListener("click", function () {
+	getHint();
+});
+btn_next.addEventListener("click", function () {
+	incrementQuestion();
+});
+document.addEventListener("DOMContentLoaded", function () {
+	routeView("landing");
+});
+document.addEventListener
